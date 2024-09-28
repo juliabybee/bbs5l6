36e3f28e546ab69c3626b09f82dcfb5a18c58738
juliabybee <106109681+juliabybee@users.noreply.github.com>
2024-09-28 13:05:46 -0500

    Add files via upload

---
+++ BBSprint5L6/script.js
@@ -0,0 +1,9 @@
+document.getElementById("myButton1").addEventListener
+("click", function () {alert("Button 1 was clicked!")});
+
+/*selects the element to add the event listener to, then adds
+//the event listener which listens for a click. When it detects
+//the click, it sends an alert that the button was clicked */
+
+document.getElementById("myButton2").addEventListener
+("click", function () {alert("Button 2 was clicked!")});
